8a8f1c982272d91a860fed6f87ca8173e8b049c6
Douglas Martins <douglas5422@outlook.com>
2022-12-29 08:44:43 -0300

    Metodo GET e POST

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.get("/teste", (req, res) => {
-    return res.json({Msg: "Servidor OK!"})
+//Pegar um dado
+app.get("/users", (req, res) => {
+
+  const users = [
+    {
+      "id": 1,
+      "name": "Usuário 1"
+    },
+    {
+      "id": 2,
+      "name": "Usuário 2"
+    }
+
+  ]
+  return res.json(users);
 });
+
+//Enviar um dado
+app.post("/users", (req, res) => {
+
+  const user = req.body;
+
+  return res.json(user);
+})
 
 app.listen(3002);
